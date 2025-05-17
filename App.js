a3db41f588fc8444fcaca86e1c81dc373eac2f9a
RoanNL <roanlisboa4@gmail.com>
2025-05-16 21:40:46 -0300

    explicação

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ import Inicio from './screens/welcomeScreen/Inicio';
 import HomeScreen from './screens/HomeScreen';
 import Quiz from './screens/quiz';
 import Perfil from './screens/perfil/Perfil';
-import Notificacoes from './screens/perfil/Notificacoes';
 import ModificarDados from './screens/perfil/ModificarDados';
 import RegistroScreen from './screens/ResgistroScreen';
 
@@ -21,7 +20,6 @@ export default function App() {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Quiz" component={Quiz} />
         <Stack.Screen name="Perfil" component={Perfil} />
-        <Stack.Screen name="Notificacoes" component={Notificacoes} />
         <Stack.Screen name="ModificarDados" component={ModificarDados} />
       </Stack.Navigator>
     </NavigationContainer>
